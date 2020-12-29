f4977954b269c085aed412a9f04905ec927130fe
ingmarinho <ingmarfontijne@hotmail.com>
2020-12-29 13:40:19 +0100

    better titlebar

--- assets/js/titlebar.js
+++ assets/js/titlebar.js
@@ -2,16 +2,21 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const titleArr = ['\\', '|', '/', 'i', 'i\\', 'i|', 'i/', 'in', 'in\\', 'in|', 'in/', 'ing', 'ing\\', 'ing|', 'ing/', 'ingm', 'ingm\\', 'ingm|', 'ingm/', 'ingma', 'ingma\\', 'ingma|', 'ingma/', 'ingmar', 'ingmar\\', 'ingmar|', 'ingmar/', 'ingmar.', 'ingmar.\\', 'ingmar.|', 'ingmar./', 'ingmar.d', 'ingmar.d\\', 'ingmar.d|', 'ingmar.d/', 'ingmar.de', 'ingmar.de\\', 'ingmar.de|', 'ingmar.de/', 'ingmar.dev'];
+const titleStartArr = ['> \\', '> |', '> /', '> i', '> i\\', '> i|', '> i/', '> in', '> in\\', '> in|', '> in/', '> ing', '> ing\\', '> ing|', '> ing/', '> ingm', '> ingm\\', '> ingm|', '> ingm/', '> ingma', '> ingma\\', '> ingma|', '> ingma/', '> ingmar', '> ingmar\\', '> ingmar|', '> ingmar/', '> ingmar.', '> ingmar.\\', '> ingmar.|', '> ingmar./', '> ingmar.d', '> ingmar.d\\', '> ingmar.d|', '> ingmar.d/', '> ingmar.de', '> ingmar.de\\', '> ingmar.de|', '> ingmar.de/', '> ingmar.dev'];
+const titleEndArr = ['> ingmar.dev', '> ngmar.dev', '> gmar.dev', '> mar.dev', '> ar.dev', '> r.dev', '> .dev', '> dev', '> ev', '> v', '>'];
 
 async function changeTitle() {
-    for (let i = 0; i < titleArr.length; i++) {
-        document.title = titleArr[i];
+    for (let i = 0; i < titleStartArr.length; i++) {
+        document.title = titleStartArr[i];
         await sleep(300);
     }
 
-    document.title = '[ ingmar.dev ]';
-    await sleep(800);
+    await sleep(200);
+
+    for (let i = 0; i < titleEndArr.length; i++) {
+        document.title = titleEndArr[i];
+        await sleep(250);
+    }
 
     return changeTitle();
 }
