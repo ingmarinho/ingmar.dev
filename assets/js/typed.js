ee73447492e6157312e7e04e8e48f4d02908f2c6
ingmarinho <ingmarfontijne@hotmail.com>
2021-05-23 17:28:59 +0200

    yes

---
+++ assets/js/typed.js
@@ -0,0 +1,43 @@
+// Typing bit
+let title = new Typed('#website-name', {
+    strings: ['<strong>Ingmar.dev</strong>'],
+    typeSpeed: 40,
+    cursorChar: '',
+    startDelay: 250
+});
+
+let title2 = new Typed('#github', {
+    strings: ['&nbsp;ingmarinho'],
+    typeSpeed: 40,
+    cursorChar: '',
+    startDelay: 400
+});
+
+let title3 = new Typed('#instagram', {
+    strings: ['&nbsp;ingm4rr'],
+    typeSpeed: 40,
+    cursorChar: '',
+    startDelay: 800
+});
+
+let title4 = new Typed('#discord', {
+    strings: ['&nbsp;Ingmar#0001'],
+    typeSpeed: 40,
+    cursorChar: '',
+    startDelay: 1200
+});
+
+let title5 = new Typed('#steam', {
+    strings: ['&nbsp;Ingmar'],
+    typeSpeed: 40,
+    cursorChar: '',
+    startDelay: 1600
+});
+
+let title6 = new Typed('#onlyfans', {
+    strings: ['&nbsp;Ingmar'],
+    typeSpeed: 40,
+    cursorChar: '',
+    startDelay: 2000
+});
+
