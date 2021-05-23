aa269d0505daca19e8cf038e6b20b2f5dee6a582
ingmarinho <ingmarfontijne@hotmail.com>
2021-05-23 18:04:21 +0200

    Update warning.js

--- assets/js/warning.js
+++ assets/js/warning.js
@@ -1,5 +1,14 @@
 function showWarning() {
-    if (confirm("18+ content, are you sure you want to continue?")) {
-        window.open("https://ingmar.dev/onlyfans", "_blank");
+    if (confirm("This page contains 18+ content, are you sure you want to continue?")) {
+        if (confirm("Are you really sure?")) {
+            if (confirm("Promise?")) {
+                if (confirm("Don't 'OK' me, who do you think you are?")) {
+                    if (confirm("It's not my problem if you get offended at this point.")) {
+                        window.open("https://ingmar.dev/onlyfans", "_blank");
+                    }
+                }
+            }
+        }
+
     }
 }
